890047306501788a7a08c780aba1588f8c93935c
sumitnair26 <sumit.nair26@gmail.com>
2024-03-09 13:04:48 +0530

    adding routes to project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,28 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/signup', (c) => {
+  return c.text('Hello Hono post! signup')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono! signin')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono! blog')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono! blog')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono! id')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Hello Hono! bulk')
+})
+
 export default app
