08f325cb9f4620e68e35d21be8de2288ccf1396d
sumitnair26 <sumit.nair26@gmail.com>
2024-03-11 00:12:46 +0530

    gitignore

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,13 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
+import z from "zod";
+
+const signupInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional()
+})
 
 // Create the main Hono app
 export const userRouter = new Hono<{
@@ -16,6 +23,13 @@ userRouter.post('/signup', async (c) => {
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
     const body = await c.req.json();
+    const { success } = signupInput.safeParse(body);
+    if (!success) {
+        c.status(411)
+        return c.json({
+            message: "Input validation failed"
+        })
+    }
     try {
       const user = await prisma.user.create({
         data:{
