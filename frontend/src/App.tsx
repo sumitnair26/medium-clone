523c5de373582292e3cadc56edadc8992d533717
sumitnair26 <sumit.nair26@gmail.com>
2024-03-11 18:48:57 +0530

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
