e4434bacb81115391765c904936f910b699e1e6f
sumitnair26 <sumit.nair26@gmail.com>
2024-03-13 23:58:56 +0530

    responsive fix for small size

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,7 +3,7 @@ import { Quote } from "../components/Quote"
 
 export const Signin = () => {
     return <div>
-                <div className="grid grid-cols-2">
+                <div className="grid grid-cols-1 lg:grid-cols-2">
                     <div>
                         <Auth type="signin" />
                     </div>
