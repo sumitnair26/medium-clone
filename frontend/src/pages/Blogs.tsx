812480c7e186ce8a24f3164f3b428a1fa57918d6
sumitnair26 <sumit.nair26@gmail.com>
2024-03-15 15:53:45 +0530

    stating with BlogCard component

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,10 @@
+import { BlogCard } from "../components/BlogCard"
+
+export const Blogs = () => {
+    return <div>
+        <BlogCard authorName="Sumit" 
+        title="aaa" 
+        content="aaaa aaa aaaa aaaaaaaaaaaaaaa" publishedDate="26/02/2024" />
+    </div>
+}
+
