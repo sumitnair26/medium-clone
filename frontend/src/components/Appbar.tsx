5a8b06021511c38d47f76216c35b1857941e00f5
sumitnair26 <sumit.nair26@gmail.com>
2024-03-16 18:19:44 +0530

    fixed nav

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4" >
-        <div>
+        <div className="flex flex-col justify-center">
             Medium Clone
         </div>
         <div>
