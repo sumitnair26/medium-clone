8d6d91b9e0ecb160c4f76477b0826d546c4e441e
sumitnair26 <sumit.nair26@gmail.com>
2024-03-19 15:49:05 +0530

    fixed console log

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -12,15 +12,14 @@ export const Appbar = () => {
             <Link to={`/Publish`} >
                 <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mr-4">New</button>
             </Link>
-            <svg className="w-8 h-8 px-px text-gray-800 cursor-pointer" onClick={Logout} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" id="logout">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"/>
+            <svg className="w-8 h-8 px-px text-gray-800 cursor-pointer" onClick={Logout} fill="none" viewBox="0 0 16 16">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"/>
             </svg>
             <Avatar name="Sumit" size="big" />
         </div>
     </div>
 
     function Logout() {
-        console.log(`Clicked Logout`);
         localStorage.clear();
         return navigate('/');
     }
