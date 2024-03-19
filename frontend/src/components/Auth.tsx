63b0c19981828c92cfbce043b2ae448985e72341
sumitnair26 <sumit.nair26@gmail.com>
2024-03-19 21:11:46 +0530

    conflicts resolved

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ChangeEventHandler, useState, React } from "react";
+import { ChangeEvent, useState, React } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signupInput } from "@sumitnair26/medium-common";
 import axios from "axios";
@@ -62,7 +62,6 @@ export const Auth = ({type}: {type:"signup" | "signin"}) => {
                                 })
                             }} />
                         </div>
-
                         <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                             {type==="signup"? "Sign up" : "Sign in"} </button>
                 </div>
